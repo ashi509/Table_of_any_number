4da6b644901c26662eef5f773dc476f16958e56c
Ashish Kumar <57478628+ashi509@users.noreply.github.com>
2023-02-20 14:10:53 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Table{
+public static void  main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your selected number whose you want to print table :");
+int x=sc.nextInt();
+for(int i=1; i<=10; i++){
+System.out.println(i*x);
+
+
+}
+}
+
+}
